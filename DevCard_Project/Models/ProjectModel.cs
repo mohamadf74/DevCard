b69b26e966b667b7d3440cc648a11a2d9b6ba574
mohamad <m.f74@hotmail.com>
2021-12-07 17:41:06 +0330

    Moving projects to view Component

--- DevCard_Project/Models/ProjectModel.cs
+++ DevCard_Project/Models/ProjectModel.cs
@@ -7,9 +7,19 @@ namespace DevCard_Project.Models
 {
     public class ProjectModel
     {
+
+        public ProjectModel(long _Id,string _Name,string _Description,string _image,string _Client)
+        {
+            Id = _Id;
+            Name = _Name;
+            Client = _Client;
+            Description = _Description;
+            Image = _image;
+        }
         public long Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string Image { get; set; }
         public string Client { get; set; }
     }
 }
---
+++ DevCard_Project/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevCard_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard_Project.ViewComponents
+{
+    public class ProjectsViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var projects = new List<ProjectModel>()
+            {
+                new ProjectModel(1, "درخواست تاکسی", "درخواست آنلاین تاکسی برای سفرهای درون شهری", "project-1.jpg","اسنپ"),
+                new ProjectModel(2, "زودفود", "درخواست آنلاین غذا برای سراسر کشور","project-2.jpg", "زودفود"),
+                new ProjectModel(3, "مدارس", "سیستم مدیریت یکپارچه مدارس","project-3.jpg", "مونوپ"),
+                new ProjectModel(4 , "فضاپیما", "برنامه مدیریت فضاپیماهای ناسا","project-4.jpg", "ناسا"),
+            };
+
+            return View("_ProjectsViewComponent",projects);
+        }
+    }
+}
